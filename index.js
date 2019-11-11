c50f41a0d8bfff67d861b5cd48aa4c0af6d7e817
Arcene LB <55070235+lbernadel@users.noreply.github.com>
2019-11-10 20:19:47 -0500

    number of followers returns properly

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ inquirer
     axios
     .get('https://api.github.com/users/' + input.username)
     .then(response =>{
-        console.log(response)
         let avatar ="![profile](" + response.data.avatar_url+")",
         displayName = "# <span style='color:" + input.color + ";'>" + response.data.name + "</span>",
         bio = "Bio: " + response.data.bio,
         userLoc = "Location: " + response.data.location,
         userRepo = "GitHub Repo: [" +response.data.name+ "'s Repo](" + response.data.html_url+")",
         publicRepos = "Public Repos: " + response.data.public_repos,
+        followers = "Followers: " + response.data.followers;
 
         const spanStart = "<span style='font-size:14pt;'>",
         spanClose = "</span>";
 
 
-        let results=displayName + "\n" + avatar + "\n\n" +spanStart + userLoc + spanClose + "\n\n"+ spanStart +bio +spanClose + "\n\n" +spanStart + userRepo + spanClose +"\n\n" + spanStart+ publicRepos + spanClose+"\n\n";
+        let results=displayName + "\n" + avatar + "\n\n" +spanStart + userLoc + spanClose + "\n\n"+ spanStart +bio +spanClose + "\n\n" +spanStart + userRepo + spanClose +"\n\n" + spanStart+ publicRepos + spanClose+"\n\n"+ spanStart+ followers+spanClose+"\n\n";
         fs.writeFile("profile.md",results, error =>{
             if(error){
                 console.log("Unfortunately, an error occurred.")
--- profile.md
+++ profile.md
@@ -1,4 +1,4 @@
-# <span style='color:purple;'>Laurie Bernadel</span>
+# <span style='color:green;'>Laurie Bernadel</span>
 ![profile](https://avatars0.githubusercontent.com/u/55070235?v=4)
 
 <span style='font-size:14pt;'>Location: Charlotte Metro Area, Charlotte, NC</span>
@@ -8,4 +8,6 @@
 <span style='font-size:14pt;'>GitHub Repo: [Laurie Bernadel's Repo](https://github.com/lbernadel)</span>
 
 <span style='font-size:14pt;'>Public Repos: 12</span>
+
+<span style='font-size:14pt;'>Followers: 1</span>
 
